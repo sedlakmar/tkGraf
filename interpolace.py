5939ef307be8c0ca71183b55531e527b1af056d0
martinsed <sed39220@spseol.cz>
2023-03-06 13:54:49 +0100

    interpolace

---
+++ interpolace.py
@@ -0,0 +1,26 @@
+import scipy.interpolate as inp
+import pylab as lab
+
+#x= [0, 0.3, 0.5, 0.8, 1,  2,  3 ]
+#y= [0, 0.1, 0.5, 1,   3, 10, 30]
+
+x= "0 0.3 0.5 0.8 1  2  3".split()
+y= "0 0.1 0.5 1   3 10 30".split()
+
+x = list(map(float, x))
+y = list(map(float, y))
+
+lab.plot(x, y, 'o', color='red')
+
+spl = inp.CubicSpline(x, y)
+newX = lab.linspace(0, 3, 200)
+newY = spl(newX)
+lab.plot(newX, newY, ':', label='CubicSpline')
+
+spl = inp.UnivariateSpline(x, y)
+newY = spl(newX)
+lab.plot(newX, newY, ':', label='make_interp_spline')
+
+lab.grid()
+lab.legend()
+lab.show()
